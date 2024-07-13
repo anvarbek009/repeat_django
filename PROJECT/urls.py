edbbe44b8b0a7ab81a81c2c00d23734415c72d1b
anvarbek009 <anvarasqarov2009@gmail.com>
2024-07-13 22:37:35 +0500

    first commit

---
+++ PROJECT/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', include('app_main.urls')),
+    path('users/', include('app_users.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+]
